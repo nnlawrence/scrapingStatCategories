c5206f06a0429a9e0e8091d76194b85aa9bada78
Nathan Lawrence <nathan.lawrence@mx.com>
2023-07-25 01:19:53 -0600

    scraping titles from stat categories

---
+++ index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import express from 'express';
+
+const port = 8000;
+
+const app = express();
+
+const url = 'https://www.nba.com/stats';
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const statCategories: {
+      title: string;
+    }[] = [];
+
+    $('.LeaderBoardCard_lbcTitle___WI9J', html).each(function () {
+      const title = $(this).text();
+
+      statCategories.push({
+        title
+      });
+    });
+    console.log(statCategories);
+    // console.log(select);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(port, () => console.log(`server running on port ${port}`));
